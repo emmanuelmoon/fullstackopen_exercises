24fc70519b43d070ec57513b1305f4ea1a86b80f
emmanuelmoon <emmenual123@gmail.com>
2023-06-04 22:24:06 +0500

    Complete Exercises 1.1 and 1.2

---
+++ courseinfo/src/App.js
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+};
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.numberOfExercises}</p>
+  )
+};
+
+const Content = (props) => {
+  return (
+    < >
+      <Part part={props.parts[0]} numberOfExercises={props.numbersOfExercises[0]} />
+      <Part part={props.parts[1]} numberOfExercises={props.numbersOfExercises[1]} />
+      <Part part={props.parts[2]} numberOfExercises={props.numbersOfExercises[2]} />
+    </>
+  )
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+};
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={[part1, part2, part3]} numbersOfExercises={[exercises1, exercises2, exercises3]} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
